1cf0ca28ee3a83b417b75c1170fb6cec2cdabe5e
Thiago Leite <thiagofmleite@gmail.com>
2023-08-15 11:37:01 -0300

    Generic Error FIlter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PrismaExceptionFilter } from './exception-filters/prisma.exception.filter';
+import { PrismaExceptionFilter } from './exception-filters/prisma.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
